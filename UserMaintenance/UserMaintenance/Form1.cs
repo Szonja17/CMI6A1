bb465c6afec759509e76eb2761372f373ea11521
Szonja17 <toke.szonja@gmail.com>
2022-09-30 13:56:15 +0200

    Hellooooooo

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -13,12 +14,26 @@ namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.LastName; 
             label2.Text = Resource1.FirstName; 
-            button1.Text = Resource1.Add; 
+            button1.Text = Resource1.Add;
+            BindingList.DataSource = users;
+            BindingList.ValueMember = "ID";
+            BindingList.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
