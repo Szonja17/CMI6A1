4b6192b07947ccb4e0645eef3af86a31e1331414
Szonja17 <toke.szonja@gmail.com>
2022-10-07 19:13:53 +0200

    Excel munkafüzet megnyitása

--- UserMaintenance/UserMaintenance/Form2.cs
+++ UserMaintenance/UserMaintenance/Form2.cs
@@ -17,11 +17,46 @@ namespace UserMaintenance
         //Nem tudok csatlakozni az adatbázishoz valamiért errort ír ki a laptopom, ezért nem jött létre a Model1-em és az Entities se.
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
-
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet; 
         public Form2()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                
+                xlApp = new Excel.Application();
+
+              
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable(); 
+
+                
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+               
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
 
         private void LoadData()
