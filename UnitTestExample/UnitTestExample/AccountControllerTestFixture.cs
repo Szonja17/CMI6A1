d5a4b0adf04e6ce37e68dd9764f63d7561f7336b
Szonja17 <toke.szonja@gmail.com>
2022-11-25 09:54:45 +0100

    elso feladat

---
+++ UnitTestExample/UnitTestExample/AccountControllerTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet.Framework;
+
+namespace UnitTestExample
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        private void TestValidateEmail(string Email, bool ExprectedResult)
+        {
+
+        }
+    }
+}
