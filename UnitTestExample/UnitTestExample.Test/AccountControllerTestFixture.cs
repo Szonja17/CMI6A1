7a69161237c4fb58a6d6ddbe188a811716d5e39b
Szonja17 <toke.szonja@gmail.com>
2022-11-25 14:23:43 +0100

    kesz

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test,
+         TestCase("abcd1234", false),
+         TestCase("irf@uni-corvinus", false),
+         TestCase("irf.uni-corvinus.hu", false),
+         TestCase("irf@uni-corvinus.hu", true)]
+        public void TestValidateEmail(string email, bool expectedResult)
+            {
+            var accountController = new AccountController();
+            var actualResult = accountController.ValidateEmail(email);
+            Assert.AreEqual(expectedResult, actualResult);
+            }
+        [Test,
+        TestCase("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", true),
+        TestCase()
+            
+            ]
+        public void TestPassword()
+        { 
+        
+        }
+        
+    }
+}
